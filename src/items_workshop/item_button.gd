8c4ffc82f6a116f29eadfd1fe0079d1e704444d8
Raul <mailctrlraul@gmail.com>
2023-06-06 06:50:48 -0300

    theme enhancements

--- src/items_workshop/item_button.gd
+++ src/items_workshop/item_button.gd
@@ -2,6 +2,11 @@ extends Button
 class_name SMItemButton
 
 
+
+@onready var texture_rect: TextureRect = %TextureRect
+
+
+
 var item: ItemDef
 
 
@@ -10,6 +15,7 @@ func _get_drag_data(_position: Vector2):
 	return DragData.new(self, item)
 
 
+
 func set_item(value: ItemDef) -> void:
 	item = value
-	icon = Assets.get_texture_for_item(item)
+	texture_rect.texture = Assets.get_texture_for_item(item)
